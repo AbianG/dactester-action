b8f802b73915a1cf10302dcf0e7a133f7fc9b1da
Abian Rodriguez <arodriguez@accela.com>
2022-04-21 05:09:54 -0600

    Update action to checkout github repo and search for parametrized folder to find tests inside

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 
-for file in $(find ./folderToTest -type f -name '*.md'); do
+echo "Specified path is"
+echo $1
+for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
             python3 app/dactester/tester.py $file || exit 1;
           done
 
-echo "Testing for workspace"
-ls /github/workspace
-cat /github/workspace/readme.md
+#echo "Testing for workspace"
+#ls /github/workspace
+#cat /github/workspace/readme.md
