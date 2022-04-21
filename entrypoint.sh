58a6c20841edbb401e045162061a994a47b5533b
Abian Rodriguez <arodriguez@accela.com>
2022-04-21 05:28:21 -0600

    Remove debug lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -l
 
-echo "Testing for workspace"
-ls /github/workspace
-ls /github/workspace/tests
-
-echo "Specified path is"
-echo $1
 for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
             python3 /app/dactester/tester.py $file || exit 1;
-          done
+done
