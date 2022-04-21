627031345725fe8bd32436db3a045359bdda6890
Abian Rodriguez <arodriguez@accela.com>
2022-04-21 04:34:26 -0600

    Add loop to get tests in entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM agrm/dactester:poc
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
+ADD /tests /folderToTest
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello"
-#time=$(date)
-#echo "::set-output name=time::$time"
+for file in $(find ./folderToTest -type f -name '*.md'); do
+            echo "> Test $file";
+            python3 app/dactester/tester.py $file || exit 1;
+          done
+
+echo "Testing for workspace"
+cat /github/workspace/README.md
