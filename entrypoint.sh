92ef370ae66e1e3c203eed4624a7dd6ee9220416
Abian Rodriguez <arodriguez@accela.com>
2022-04-25 08:53:41 -0600

    Add ruleconfig.yml as parameter to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
-            python3 /app/dactester/tester.py $file || exit 1;
+            python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml || exit 1;
 done
