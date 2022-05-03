74d9cbc8e15508f58d1e2de5658db13e265249fe
Abian Rodriguez <arodriguez@accela.com>
2022-05-03 02:47:55 -0600

    Correct output variable to be used as bash array

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
-            output=$(python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml) || exit 1;
+            output+=$(python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml) || exit 1;
             echo "::set-output name=results::$output"
 done
