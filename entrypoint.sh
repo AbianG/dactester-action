2b45b2e156a37e3451da4a81803e8741a6625d6a
Abian Rodriguez <arodriguez@accela.com>
2022-05-03 03:06:10 -0600

    Capture whole for loop output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
-output=" "
-for file in $(find /github/workspace/$1 -type f -name '*.md'); do
+
+output=$(
+    for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
-            output+=$(python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml) || exit 1;
+            python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml || exit 1;
             echo "::set-output name=results::$output"
-done
+    done
+)
