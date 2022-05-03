3eb3af4dc9db83112ca21c202866243a304457a8
Abian Rodriguez <arodriguez@accela.com>
2022-05-03 03:09:44 -0600

    Move echo of output outside loop capturing output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ output=$(
     for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
             python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml || exit 1;
-            echo "::set-output name=results::$output"
     done
 )
+echo "::set-output name=results::$output"
