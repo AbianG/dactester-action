38bf30421af44f20d09a8dc0c5892bf5f9f1753a
Abian Rodriguez <arodriguez@accela.com>
2022-05-03 02:50:19 -0600

    Add missing array declaration at start of entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-
+output=()
 for file in $(find /github/workspace/$1 -type f -name '*.md'); do
             echo "> Test $file";
             output+=$(python3 /app/dactester/tester.py $file /github/workspace/ruleconfig.yml) || exit 1;
